14ecf69c20fb9a7723e53fb47516f870d79b3656
sankar <bikerer17@gmail.com>
2023-05-31 22:07:45 +0530

    adding train tracking project file

---
+++ javainterview/Main.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class a {
+    public a() {
+        System.out.println("obj created");
+    }
+
+}
+
+public class Main {
+    public public static void main(String[] args) throws Exception {
+        a obj = new a();
+    }
+}
